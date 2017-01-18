553f529fe52de9b783c7390a05bb172442162b3c
amnhh <2570079254@qq.com>
2017-01-18 14:40:35 +0800

    stop ajax

--- src/ajax.js
+++ src/ajax.js
@@ -4,3 +4,12 @@
  * Mail : amnhhlod@gmail.com
  */
 
+var Amnhh = require('./core');
+
+require('./promise');
+
+Amnhh.ajax = function (config) {
+  return new Amnhh.fn.Promise(function (resolve, reject) {
+
+  });
+};
